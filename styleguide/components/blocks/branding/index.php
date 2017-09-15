ad648eccb8719c2f60f28ca7c5c63be739727769
Mark Conroy <info@adesignforlife.net>
2017-09-15 16:25:33 +0100

    html for branding block

---
+++ styleguide/components/blocks/branding/index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file
+ * This file creates the branding block.
+ */
+?>
+
+<style>
+  <?php include 'style.css'; ?>
+</style>
+
+<script>
+  <?php include 'script.js'; ?>
+</script>
+
+<div id="block-branding" class="clearfix site-branding block block-system block-system-branding-block">
+  <a href="/" title="Home" rel="home" class="site-branding__logo">
+    <img src="/core/themes/bartik/logo.svg" alt="Home">
+  </a>
+  <div class="site-branding__text">
+    <div class="site-branding__name">
+      <a href="/" title="Home" rel="home">Umami</a>
+    </div>
+  </div>
+</div>
