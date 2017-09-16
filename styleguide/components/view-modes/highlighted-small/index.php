c9ac206c22b3d0c1b47b26545223d1edb3fad702
Mark Conroy <info@adesignforlife.net>
2017-09-16 19:41:11 +0100

    adding html for highlighted* view modes

--- styleguide/components/view-modes/highlighted-small/index.php
+++ styleguide/components/view-modes/highlighted-small/index.php
@@ -13,3 +13,18 @@
 <script>
   <?php include 'script.js'; ?>
 </script>
+
+<article role="article" class="node node--type-recipe node--view-mode-highlighted-small" about="/pages/recipe">
+  <div class="node__content">
+    <div class="field field--name-field-image field--type-image field--label-hidden field__item">
+      <a href="/pages/recipe" rel="bookmark">
+        <img src="/sites/default/files/styles/large/public/2017-09/recipe-img-5_4.png?itok=aMpnj6oS" alt="Image of Sample Recipe" typeof="foaf:Image" class="image-style-large">
+      </a>
+    </div>
+    <h2>
+      <a href="/pages/recipe" rel="bookmark">
+        <span class="field field--name-title field--type-string field--label-hidden">A Sample Recipe</span>
+      </a>
+    </h2>
+  </div>
+</article>
