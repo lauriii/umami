2229b1b124b8ba5ab63dc38264d13ed7668e63b6
Mark Conroy <info@adesignforlife.net>
2017-09-24 09:20:44 +0100

    adding/mapping regions for drupal and adding page.html.twig

--- styleguide/components/regions/header/index.php
+++ styleguide/components/regions/header/index.php
@@ -20,7 +20,8 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/components/regions/top/index.php'; ?>
 
 <header class="header" role="banner">
-  <!-- include the header items here - search block, login, menu, logo, etc -->
-  <?php include $_SERVER['DOCUMENT_ROOT'] . '/components/blocks/branding/index.php'; ?>
-  <?php include $_SERVER['DOCUMENT_ROOT'] . '/components/navigation/menu--main/index.php'; ?>
+  <div class="region region-header container">
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/components/blocks/branding/index.php'; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/components/navigation/menu--main/index.php'; ?>
+  </div>
 </header>
--- styleguide/components/regions/top/index.php
+++ styleguide/components/regions/top/index.php
@@ -7,15 +7,17 @@
 
 <style>
   <?php include $_SERVER['DOCUMENT_ROOT'] . '/base.css'; ?>
-  <?php include 'style.css'; ?>
+  <?php include $_SERVER['DOCUMENT_ROOT'] . '/components/regions/top/style.css'; ?>
 </style>
 
 <script>
   <?php include 'script.js'; ?>
 </script>
 
-<section class="region region-top">
-  <!-- include the region-top items here - search block, login, etc-->
-  <?php include $_SERVER['DOCUMENT_ROOT'] . '/components/blocks/search/index.php'; ?>
-  <h2>Login</h2>
+<section class="layout-top">
+  <div class="region region-top container">
+    <!-- include the region-top items here - search block, login, etc-->
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/components/blocks/search/index.php'; ?>
+    <h2>Login</h2>
+  </div>
 </section>
