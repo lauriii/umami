0a2e4fc6d65972d2290a3ac22d72ea752a0e820a
Lauri Eskola <lauri.eskola@gmail.com>
2017-09-28 13:29:12 +0300

    KSS styleguide

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var path = require('path');
+var del = require('del');
+var runSequence = require('run-sequence');
+var kss = require('kss');
+
+var options = {};
+
+options.rootPath = {
+    components: __dirname + '/components/',
+    styleGuide: __dirname + '/styleguide/'
+};
+
+options.styleGuide = {
+    source: [options.rootPath.components],
+    builder: 'builder/twig',
+    namespace: 'umami',
+    destination: options.rootPath.styleGuide,
+    mask: /\.css/,
+    css: [options.rootPath.styleGuide + 'assets/styles.css'],
+    js: [],
+    homepage: 'homepage.md',
+    title: 'Umami Styleguide'
+};
+
+gulp.task('clean', function() {
+    return del(['styleguide']);
+});
+gulp.task('styles', function() {
+    return gulp.src(options.rootPath.components + '**/*.css')
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest(options.rootPath.styleGuide + 'assets'));
+});
+gulp.task('styleguide', ['clean', 'styles'], function() {
+    return kss(options.styleGuide);
+});
+gulp.task('images', function() {
+    gulp.src(['styles/images/**']).pipe(gulp.dest('styleguide/public/images'));
+});
+gulp.task('javascript', function() {
+    gulp.src(['styles/**/*.js']).pipe(gulp.dest('styleguide/public/javascript'));
+});
+
+gulp.task('default', function() {
+    runSequence(['clean', 'styleguide'], 'images', 'javascript');
+});
