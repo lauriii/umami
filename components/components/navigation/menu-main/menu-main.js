2b93899c390abb76f100e640f9098b193ff834ab
Tamás Hajas <hajastamas@gmail.com>
2017-10-18 07:51:21 +0200

    #49 Menu toggle function for narrower screns
    Currently broken in KSS see https://codepen.io/thamas/pen/BwGWGR instead

--- components/components/navigation/menu-main/menu-main.js
+++ components/components/navigation/menu-main/menu-main.js
@@ -2,3 +2,19 @@
  * @file
  * This file is used to add any javascrip that is needed for the main menu.
  */
+
+(function ($, Drupal) {
+  'use strict';
+  Drupal.behaviors.menutoggle = {
+    attach: function (context, settings) {
+      var toggler = $('.menu-main__toggle');
+      var menu = $('.menu-main');
+
+      toggler.click(function toggle () {
+        toggler.toggleClass('is-active');
+        menu.toggleClass('is-active');
+        return false;
+      });
+    }
+  };
+})(jQuery, Drupal);
