f78b8a9ed5dca9da03b2bdbf4cd1915718f661e6
Tamás Hajas <hajastamas@gmail.com>
2017-10-24 18:28:50 +0200

    Main menu (#80)
    * Fix jumping text for now
    
    * #36 Prevent unvanted duplication caused by modifier misuse.
    
    * Set the markup to be as it comes from classy.
    
    * #36 Hide what we do not need and set placeholder text.
    
    * #36 Style placeholder text
    
    * #36 Add search icon and finish styling
    
    * #36 Move visually hidden to base.css
    
    * View mode highlighted small (#69)
    
    * adding colours to allow us test for ally contrast
    
    * adding tpl file for colours
    
    * first commit of banner block using kss
    
    * first commit of branding block using kss
    
    * logo has been moved to the branding block directory
    
    * first commit of image styles using kss
    
    * first commit of view modes using kss
    
    * initial commit of styling body elements, we should break these into separate files
    
    * move each html text element to its own component
    
    * setting order for typography items
    
    * move body element styling back to base.css, remove body directory from typography directory
    
    * initial commit of form items directory, needed for the field label states
    
    * committing styling for the highlighted small view mode
    
    * small tweaks from pull request feedback
    
    * small tweaks from pull request feedback, volume 2
    
    * finalising hover states
    
    * #49 There is no breadcrumb in the design so delete the related component
    
    * #49 Create basic structure
    
    * #49 Styling and layout for larger screens
    
    * #49 Add some responsive behavior
    
    * #49 Add a media query
    
    tune it later when building the region
    
    * #49 Add toggle button
    
    * #49 Add hamburger icon (compressed svg :))
    
    * #49 Menu toggle function for narrower screns
    
    Currently broken in KSS see https://codepen.io/thamas/pen/BwGWGR instead
    
    * #49 Use inline svg instead of background image
    
    * #49 Change rule order
    
    See: https://github.com/lauriii/umami/pull/80#pullrequestreview-70110011
    
    * #49 Change rule order
    
    See: https://github.com/lauriii/umami/pull/80#discussion_r145328762
    
    * #49 Use json source instead of hardcoded data
    
    See: https://github.com/lauriii/umami/pull/80#discussion_r145328511
    
    * #49 We have to have just .twig as file extension (not .html.twig)!
    
    …to be able to embed the json data into the template
    
    See the previous 9a0a546 commit too
    
    * #49 Update to the (new) narrow screen design
    
    See: https://github.com/lauriii/umami/pull/80#discussion_r145328409
    
    * #49 We should use data attributes as js selectors in Drupal
    
    See:
    - https://github.com/lauriii/umami/pull/80#discussion_r145328621 and
    - https://github.com/lauriii/umami/pull/80#discussion_r145328634
    
    * #49 Higher specificity is justifiable here
    
    * #49 Typo fix
    
    * #49 Cleanup

---
+++ components/components/navigation/menu-main/menu-main.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * This file is used to add any javascrip that is needed for the main menu.
+ */
+
+(function ($, Drupal) {
+  'use strict';
+  Drupal.behaviors.menutoggle = {
+    attach: function (context, settings) {
+      var toggler = $('[data-drupal-selector="menu-main__toggle"]');
+      var menu = $('[data-drupal-selector="menu-main"]');
+
+      toggler.click(function toggle () {
+        toggler.toggleClass('menu-main__toggle--active');
+        menu.toggleClass('menu-main--active');
+        return false;
+      });
+    }
+  };
+})(jQuery, Drupal);
